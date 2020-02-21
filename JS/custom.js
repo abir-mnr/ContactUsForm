a9ced2e66c4a02fe5a42d62b38de4ce5b42061fe
Abir <mnrabir@gmail.com>
2020-02-21 12:06:00 +0600

    After all the data validation before working for sql injection validation

--- JS/custom.js
+++ JS/custom.js
@@ -4,16 +4,39 @@ function printMessage(elementId, msg){
 function validateForm(){
   var name = document.contactUs.name.value;
   var email = document.contactUs.email.value;
-  var message = document.contactUs.message.value;
+  var mess = document.contactUs.mess.value;
+  var option=document.getElementsByName('Gender');
+
   var msg="";
+
   if(name==""){
     msg = "Please type your name";
     printMessage("nameErr", msg);
     $('#nameErr').fadeIn().delay(3000).fadeOut();
     return false;
   }
-}
 
-// setTimeout(function() {
-//    $('#nameErr').fadeOut().text('')
-// }, 5000 );
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    
+  }else {
+    msg = "Please type a valid email address";
+    printMessage("emailerr", msg);
+    $('#emailerr').fadeIn().delay(3000).fadeOut();
+    return false;
+  }
+
+  if (!(option[0].checked || option[1].checked)) {
+    msg = "Please select one";
+    printMessage("radioerr", msg);
+    $('#radioerr').fadeIn().delay(3000).fadeOut();
+    return false;
+  }
+
+  if(mess==""){
+    msg = "Please type something";
+    printMessage("messerr", msg);
+    $('#messerr').fadeIn().delay(3000).fadeOut();
+    return false;
+  }
+  return true;
+}
